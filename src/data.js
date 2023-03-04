c12597f5720e4f1372eb1b75736dc19d5f5334f7
Leftjose <87351665+Leftjose@users.noreply.github.com>
2023-03-04 13:12:56 -0600

    adding starter files

---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const projects = [
+  {
+    title: "Parece Que Va Llover",
+    subtitle: "Weather App",
+    description:
+      "The user is able to search for there city and weather information will be displayed. Technologies used: JavaScript, HTML, CSS, Bootstrap, Api's. Visit: https://openweathermap.org/ for an API key.",
+    image: "./pareceQuePIC.png",
+    link: "https://github.com/Leftjose/bug-free-octo-fiesta",
+  },
+  {
+    title: "PrimeTime",
+    subtitle: "Yelp API / Google Maps API",
+    description:
+      "This application allows the user to look for reaturants near them using google maps built into the app with Google maps API.",
+    image: "./primetime.png",
+    link: "https://fons3517.github.io/Project1-PrimeTime/",
+  },
+  {
+    title: "Password Generator",
+    subtitle: "Password App",
+    description:
+      "This Random password generator allows the user to create a password with a variety of characters and the user will also be able to copy the password to clipboard.",
+    image: "./passwordgen.png",
+    link: "https://leftjose.github.io/passwordGenerator02-25/",
+  },
+  {
+    title: "Error 404",
+    subtitle: "Responsive 404",
+    description:
+      "This 404 page is responsive with a floating ghost svg image. This is a tutorial from youtube with my own changes.",
+    image: "404Error.png",
+    link: "https://leftjose.github.io/404Responsive/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "./london.svg",
+    name: "",
+    company: "",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Pending",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React.js",
+  "Next.js",
+  "Node",
+  "Bootstrap",
+  "Git Bash",
+  "Github",
+  "Heroku",
+  "Netlify",
+];
